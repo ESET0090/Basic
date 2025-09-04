1ced603cc43a7f329aa2ba79aa781125adbed8d0
ESET0090 <gaurav.sonawane@esyasoft.com>
2025-09-04 19:53:51 +0530

    Add files via upload

---
+++ Basic of Python/Lecture 3/moduledemo.py
@@ -0,0 +1,10 @@
+
+import Calculator
+
+result = Calculator.calculator(5, 3)
+
+print("All results:", result)
+print("Addition:", result[0])
+print("Subtraction:", result[1])
+print("Multiplication:", result[2])
+print("Division:", result[3])
