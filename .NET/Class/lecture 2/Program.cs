1f4e891c2383e8b026d64836b09feb1a547bbbd2
ESET0090 <gaurav.sonawane@esyasoft.com>
2025-09-15 15:39:47 +0530

    Add files via upload

---
+++ .NET/Class/lecture 2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace class_demo
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Student second = new 
+            Student first = new Student();
+            first.initialize();
+            first.showDisplay();
+            first.name = "abcd";
+
+
+
+
+
+        }
+    }
+}
---
+++ .NET/Class/lecture 2/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_demo
+{
+    internal class Student
+    {
+        private int student_id;
+        public string name;
+        int age;
+        string contactno;
+        String emailid;
+
+        public void initialize()
+        {
+            student_id = 10;
+            name = "abcd";
+        }
+
+        public void showDisplay()
+
+        {
+
+            Console.WriteLine("Displaying object");
+            Console.WriteLine(name);
+        
+        }
+
+        public Student(int id, string std_name, int age, string contactno, string emailid)
+
+        {
+            student_id = id;
+            name = std_name;
+
+
+        }
+
+        public Student()
+        {
+
+
+
+        }
+
+        public void getTotal()
+        {
+
+
+
+        }
+
+        public void getAverage()
+        {
+
+
+        }
+
+        public void grade()
+
+        {
+
+
+
+        }
+
+    }
+}
