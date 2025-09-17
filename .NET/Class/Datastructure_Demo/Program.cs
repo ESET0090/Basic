99f3b97918dcb07de27b84e20b320ca468fe1498
ESET0090 <gaurav.sonawane@esyasoft.com>
2025-09-17 13:02:05 +0530

    Add files via upload

---
+++ .NET/Class/Datastructure_Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Datastructure_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> List_of_Student = new List<Student>();
+
+            List_of_Student.Add(new Student("Gaurav", 24));
+            List_of_Student.Add(new Student("Yash", 20));
+            List_of_Student.Add(new Student("Vinod", 27));
+
+            Console.WriteLine("The List of Students:");
+
+            foreach (Student student in List_of_Student)
+            {
+                Console.WriteLine($"Name: {student.Name}, Age: {student.Age}");
+            }
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
