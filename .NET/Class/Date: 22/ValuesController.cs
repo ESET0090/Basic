8e40ce835d4fa0b586dafce38cb63d141d69b43f
ESET0090 <gaurav.sonawane@esyasoft.com>
2025-09-22 15:46:57 +0530

    Add files via upload

---
+++ .NET/Class/Date: 22/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+public class ValuesController : ControllerBase
+    {
+        //[Route]
+    }
+};
+
+private List<string> students = new List<string>();
+{
+    "abcd",
+    "Sachin",
+    "rahul",
+};
+
+private List<Dictionary<string, string>> student_dict = new List<Dictionary<string, string>>
+{
+    new Dictionary<string, string>{{"id","1"},{"name","Sachin"}},
+    new Dictionary<string, string>{{"id","2"},{"name","Rahul"}},
+};
+
+[HttpGet]
+
+public IActionResult GetAllStudents()
+{
+    return new JsonResult(student_dict);
+};
+
+
+
